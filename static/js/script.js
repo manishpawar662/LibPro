9b4064da5d34e149a855842e6b423c8aa0203e90
manishpawar662~ <manishpawar2603@gmail.com>
2024-03-05 12:09:43 -0800

    new commit

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,29 @@
 console.log("Hii")
 
-const input = document.getElementsByTagName('input');
-input.addEventListener('keyup', function() {
-    const query = input.value;
-    console.log(query);
+// const input = document.getElementsByTagName('input');
+// input.addEventListener('keyup', function() {
+//     const query = input.value;
+//     console.log(query);
 
-    fetch('/search', {
-        method: 'POST',
-        body: query
+//     fetch('/search', {
+//         method: 'POST',
+//         body: query
+//     });
+// });
+$(document).ready(function() {
+    $('#searchbook').on('input', function() {
+        console.log(($(this).val()))
+        var searchValue = $(this).val();
+        $.ajax({
+            url: '/search/books',
+            type: 'POST',
+            data: { searchitem: searchValue },
+            success: function(response) {
+                // Handle the response from Flask (if needed)
+            },
+            error: function(xhr, status, error) {
+                // Handle errors
+            }
+        });
     });
 });
